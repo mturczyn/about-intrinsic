11c5e14951a21480b980b0f6baaa076703fe2b6b
Michal Turczyn <turek1992@o2.pl>
2024-03-10 19:03:43 +0100

    Refactored unit test for tech stack.

--- src/TechStack/TechStack.test.tsx
+++ src/TechStack/TechStack.test.tsx
@@ -1,17 +1,20 @@
 import { render } from '@testing-library/react'
 import TechStack, { PAGE_TITLE } from './index'
 
-global.window.matchMedia =
-    global.window.matchMedia ||
-    function () {
-        return {
-            matches: false,
-            addListener: function () {},
-            removeListener: function () {},
-        }
-    }
-
 test('Sets correct title of the page', () => {
+    Object.defineProperty(window, 'matchMedia', {
+        writable: true,
+        value: jest.fn().mockImplementation((query) => ({
+            matches: false,
+            media: query,
+            onchange: null,
+            addListener: jest.fn(), // Deprecated
+            removeListener: jest.fn(), // Deprecated
+            addEventListener: jest.fn(),
+            removeEventListener: jest.fn(),
+            dispatchEvent: jest.fn(),
+        })),
+    })
     render(<TechStack />)
     expect(global.window.document.title).toBe(PAGE_TITLE)
 })
