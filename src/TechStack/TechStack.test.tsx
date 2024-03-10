b6f1e31fd759d479d3f13605f1ad072c16ca59e7
Michal Turczyn <turek1992@o2.pl>
2024-03-10 19:00:24 +0100

    Corrected unit test.

--- src/TechStack/TechStack.test.tsx
+++ src/TechStack/TechStack.test.tsx
@@ -1,6 +1,16 @@
 import { render } from '@testing-library/react'
 import TechStack, { PAGE_TITLE } from './index'
 
+global.window.matchMedia =
+    global.window.matchMedia ||
+    function () {
+        return {
+            matches: false,
+            addListener: function () {},
+            removeListener: function () {},
+        }
+    }
+
 test('Sets correct title of the page', () => {
     render(<TechStack />)
     expect(global.window.document.title).toBe(PAGE_TITLE)
