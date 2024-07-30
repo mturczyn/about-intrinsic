e478bc53cc2793f7f927b737f211983b949115df
Michal Turczyn <turek1992@o2.pl>
2024-07-30 11:24:08 +0200

    Test app code change to verify all work on CICD did not break anything.

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -32,6 +32,7 @@ const Home = () => {
                         alt="me"
                     />
                     <h1>{t('helloWithIntro')}</h1>
+                    <h1>Did i break my initial pipeline?</h1>
                     <p>{t('homePageIntroduction.firstPara')}</p>
                     <div
                         className="programming-languages"
