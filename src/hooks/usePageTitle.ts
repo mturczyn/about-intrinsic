a7daf5736ed1c3a861b329e26dc12ec3a337b6e5
Michal Turczyn <turek1992@o2.pl>
2024-12-16 11:00:01 +0100

    Corrected usePageTitle hook to use useLayoutEffect in order to improve SEO

--- src/hooks/usePageTitle.ts
+++ src/hooks/usePageTitle.ts
@@ -1,7 +1,7 @@
-import { useEffect } from 'react'
+import { useLayoutEffect } from 'react'
 
 export const usePageTitle = (title: string) => {
-    useEffect(() => {
+    useLayoutEffect(() => {
         document.title = title
     }, [title])
 }
